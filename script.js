0411118b2d990fd80a295d80b34a93636db26da0
Eugene Esperas <eugene.esperas05@gmail.com>
2024-09-07 02:13:55 +0800

    done, i cheated, play aain was just refresh() page haha

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-/* --------unlock the Lock Game---------- */
+// --unlock-game
 document.addEventListener("DOMContentLoaded", () => {
+  // --show instructions
+  const instructionContainer = document.querySelector(".instruction-container");
+  const instructionBtn = document.querySelector(".instruction-btn");
+  instructionBtn.addEventListener("mouseover", () => {
+    console.log("im");
+    instructionContainer.style.display = "flex";
+  });
+
+  instructionBtn.addEventListener("mouseout", () => {
+    console.log("im");
+    instructionContainer.style.display = "none";
+  });
+
+  // let passwordKey = [];
+  // let passwordKeyToString = "";
+  let counter = 0;
+
   const gamePlay = () => {
-    // [condition para sa aking max number of input = 1]
-    const inputValue = document.querySelectorAll(".digit-value");
+    // [condition para sa aking max number of input = 4]
+    const inputguess = document.querySelector(".input-guess");
 
-    inputValue.forEach((input) => {
-      input.addEventListener("input", function () {
-        this.value = this.value.replace(/[^0-9]/g, "");
+    inputguess.addEventListener("input", function () {
+      this.value = this.value.replace(/[^0-9]/g, "");
 
-        if (this.value.length > 1) {
-          this.value = this.value.slice(0, 1);
-        }
-      });
+      if (this.value.length > 4) {
+        this.value = this.value.slice(0, 4);
+      }
     });
 
+    const displayedKey = document.querySelector(".secret-key");
     // [code para maka generate ng 4 digits secret keys]
-    const secretKeys = document.querySelector(".secret-keys");
 
     const secretfourDigitKeys = () => {
       const secretKeysArray = [];
@@ -23,133 +38,144 @@ document.addEventListener("DOMContentLoaded", () => {
         const random = Math.floor(Math.random() * 10);
         secretKeysArray.push(random);
       }
-      const secretToString = secretKeysArray.map((a) => a).join("");
-      // secretKeys.textContent = secretToString;
-      // console.log(secretToString);
-
-      return secretToString;
+      return secretKeysArray;
     };
 
-    // [para ma disable and input fields and Game Logic]
+    // --array type for keys
+    let passwordKey = secretfourDigitKeys();
+    // --string type for keys
+    const passwordKeyToString = passwordKey.join("");
+    console.log(`here ${passwordKeyToString}`);
+
+    counter = 0;
+
+    console.log(passwordKey);
 
     const enterBtn = document.querySelector(".btn");
-
-    let counter = 1;
-    const passkey = secretfourDigitKeys();
-    console.log(`passkey: ${passkey}`);
-    // ---tatanggalin ko rin itong code navigator, cheat code for debugging pa
-    const cheat = document.querySelector(".cheat");
-    cheat.textContent = passkey;
-    // ---tatanggalin ko rin itong code navigator, cheat code for debugging pa
-
+    // --kaya may false dito even default naman and false, sa line 93 nagdisabled AudioWorklet, kaya need ma enabled
     enterBtn.addEventListener("click", () => {
-      if (counter <= 5) {
-        let arrayGuest = [];
-        let newArr;
+      const inputtedguessValue = inputguess.value;
+      const arrayInputguess = inputguess.value.split("").map(Number);
+      // console.log(arrayInputguess);
 
-        // [logic para sa input fields]
-        document
-          .querySelectorAll(`.row${counter} .row-${counter}`)
-          .forEach((input) => {
-            arrayGuest.push(input.value);
-          });
+      const myImg = document.querySelector(".myImg");
+      const appendContainer = document.querySelector(".append-div-container");
+      const popupDisplayedKeys = document.querySelector(".popup-password");
+      const symbolContainer = document.querySelector(".symbol-main-container");
+      const symbol = document.querySelector(".symbol");
 
-        // [para iconvert sa string and array]
-        newArr = arrayGuest.map((a) => a).join("");
-
-        if (newArr.length == 4) {
-          // [disabling and enabling buttons]
-          document
-            .querySelectorAll(`.row${counter} .row-${counter}`)
-            .forEach((input) => (input.disabled = true));
-          document
-            .querySelectorAll(`.row${counter + 1} .row-${counter + 1}`)
-            .forEach((input) => (input.disabled = false));
-
-          counter++;
+      if (counter < 10) {
+        inputguess.value = "";
+        // --validation para sa input, dapat 4 digits lang ang pwede ma enter
+        if (inputtedguessValue.length < 4) {
+          alert("Enter Four Digit Number");
+          return;
         } else {
-          alert("Enter Four Digit Guest");
+          // [code para sa color indicator ng input fields, green yellow or default]
+          // --green if correct number and position
+          // --yellow if correct number but wrong position
+          // --default or gray if incorrect
+          const colorIndicator = () => {
+            const appendDiv = document.createElement("div");
+            arrayInputguess.forEach((input, index) => {
+              let span = document.createElement("span"); // Create a span para pag lagyan ng input
+
+              if (input == passwordKey[index]) {
+                span.style.backgroundColor = "#09fa1e"; // Correct number and position
+              } else if (passwordKey.includes(input)) {
+                span.style.backgroundColor = "#f5f247"; // Correct number, wrong position
+              } else {
+                span.style.backgroundColor = "gray"; // Wrong number
+              }
+
+              span.textContent = input; // Set the text of the span to the input value
+              appendDiv.appendChild(span); // Append the span to the appendDiv
+            });
+
+            appendDiv.classList.add("append"); // append class dynamically added sa appenDiv
+            appendContainer.appendChild(appendDiv); // yung mga guess ay nasa appendDiv tas ilalagay sa appendContainer
+          };
+
+          colorIndicator();
         }
 
-        const resultMsg = document.querySelector(".result-message");
-        const revealPass = document.querySelector(".password-value");
-        if (counter == 6) {
-          const popupResultContainer =
-            document.querySelector(".result-container");
+        inputguess.value = "";
 
-          popupResultContainer.style.display = "flex";
-          counter = 1;
-          revealPass.textContent = "WRONG GUEST";
-          resultMsg.textContent = "Better Luck Next Time!";
-        }
-
-        // --condition para if correct or wrong guest
-
-        if (newArr == passkey) {
-          const myImg = document.querySelector(".myImg");
+        // --comparison if correct and guess sa password
+        if (passwordKeyToString == inputtedguessValue) {
+          const popupContainer = document.querySelector(
+            ".popup-main-container"
+          );
           myImg.src = "assets/blueunlock.gif";
-          const popupResultContainer =
-            document.querySelector(".result-container");
+          displayedKey.textContent = passwordKeyToString;
+          popupDisplayedKeys.textContent = passwordKeyToString;
+          popupDisplayedKeys.style.color = "#55f755";
+          const popupMsg = document.querySelector(".popup-message");
+          popupDisplayedKeys;
+          popupMsg.textContent = "Good Job! Correct Guess!";
 
-          popupResultContainer.style.display = "flex";
-          counter = 1;
-          revealPass.textContent = passkey;
-          secretKeys.textContent = passkey;
-          resultMsg.textContent = "Correct Guest! Good Job!";
+          // -popup symbol check(✔) for 1000ms
+          setTimeout(() => {
+            symbol.textContent = "✔";
+            symbol.style.color = "#55f755";
+            symbolContainer.style.display = "flex";
+          }, 800);
+          enterBtn.disabled = true;
+          setTimeout(() => {
+            symbolContainer.style.display = "none";
+            popupContainer.style.display = "flex";
+          }, 1400);
         }
 
-        // [code para sa color indicator ng input fields, green yellow or default]
-        // --green if correct number and position
-        // --yellow if correct number but wrong position
-        // --default or gray if incorrect
-        const colorIndicator = () => {
-          let arrayPasskeys = passkey.split("");
-          console.log(arrayPasskeys);
-          console.log(arrayGuest);
-          if (counter <= 5) {
-            document
-              .querySelectorAll(`.row${counter - 1} .row-${counter - 1}`)
-              .forEach((input) => {
-                if (input.value == arrayPasskeys[counter - 1]) {
-                  input.style.backgroundColor = "green";
-                } else if (arrayPasskeys.includes(input.value)) {
-                  input.style.backgroundColor = "yellow";
-                } else {
-                  input.style.backgroundColor = "gray";
-                }
-              });
-          }
-        };
-        colorIndicator();
+        if (counter == 9) {
+          const popupContainer = document.querySelector(
+            ".popup-main-container"
+          );
+          displayedKey.textContent = passwordKeyToString;
+          popupDisplayedKeys.textContent = inputtedguessValue;
+          popupDisplayedKeys.style.color = "#f73131";
+          const popupMsg = document.querySelector(".popup-message");
+          popupMsg.textContent = "Better Luck Next Time!";
+          enterBtn.disabled = true;
+
+          // -popup symbol check(✗) for 1000ms
+          setTimeout(() => {
+            symbol.textContent = "✗";
+            symbol.style.color = "#f73131";
+            symbolContainer.style.display = "flex";
+          }, 800);
+          enterBtn.disabled = true;
+          setTimeout(() => {
+            symbolContainer.style.display = "none";
+            popupContainer.style.display = "flex";
+          }, 1400);
+        }
+        console.log(`counter = ${counter}`);
+        counter++;
+        console.log(inputtedguessValue.length);
       }
     });
   };
   gamePlay();
 
-  // [code para sa play again button, div will popup if correct guest or out of turn]
-  const replayBtn = document.querySelector(".replay-btn");
-  const allInputFields = document.querySelectorAll(`.key-rows input`);
+  // ---code para sa play again and pag diplay ng result
 
-  replayBtn.addEventListener("click", () => {
-    gamePlay();
-    const popupResultContainer = document.querySelector(".result-container");
-    const secretKeys = document.querySelector(".secret-keys");
-    allInputFields.forEach((input) => (input.style.backgroundColor = "#fff"));
+  // const playAgain = () => {
+  //   const playAgainBtn = document.querySelector(".play-again-btn");
+  //   const popupContainer = document.querySelector(".popup-main-container");
+  //   const enterBtn = document.querySelector(".btn");
+  //   const appendContainer = document.querySelector(".append-div-container");
+  //   const inputguess = document.querySelector(".input-guess");
 
-    popupResultContainer.style.display = "none";
-    secretKeys.textContent = "????";
-    for (let i = 1; i <= 5; i++) {
-      document.querySelectorAll(`.row1 .row-1`).forEach((input) => {
-        input.value = "";
-        input.disabled = false;
-      });
-
-      document
-        .querySelectorAll(`.row${i + 1} .row-${i + 1}`)
-        .forEach((input) => {
-          input.value = "";
-          input.disabled = true;
-        });
-    }
-  });
+  //   playAgainBtn.addEventListener("click", () => {
+  //     popupContainer.style.display = "none";
+  //     enterBtn.disabled = false;
+  //     appendContainer.innerHTML = "";
+  //     gamePlay();
+  //   });
+  // };
+  // playAgain();
+  document.querySelector(".play-again-btn").onclick = function () {
+    location.reload();
+  };
 });
